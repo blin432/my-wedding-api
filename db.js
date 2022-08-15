8c6e5eff77e9001fbe7a48ef6ef973ce7787aa0d
benjamin lin <blin432@gmail.com>
2022-08-14 20:54:03 -0400

    changed connection pool

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
-// const Pool = require("pg").Pool;
-// const pool = new Pool({
-//     user:process.env.user,
-//     password:process.env.password,
-//     database:process.env.database,
-//     host:process.env.host,
-//     port:process.env.port
-// })
-
-let connString = process.env.DATABASE_URL 
-const { Pool } = require('pg');
-console.log('process',process.env.DATABASE_URL);
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  connectionString : connString
-});
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database,
+    host:process.env.host,
+    port:process.env.port
+})
+
+// let connString = process.env.DATABASE_URL 
+// const { Pool } = require('pg');
+// console.log('process',process.env.DATABASE_URL);
+// const pool = new Pool({
+//   connectionString : connString
+// });
 module.exports = pool;
